4ce31569f2475ff78034f0cd43035b64a319ca45
oanchatw <54613208+oanchatw@users.noreply.github.com>
2026-03-22 12:44:01 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="app-layout">
-    <AppSidebar />
+    <button class="hamburger" @click="sidebarOpen = !sidebarOpen" :aria-label="sidebarOpen ? 'Close menu' : 'Open menu'">
+      <span :class="['ham-icon', { open: sidebarOpen }]">
+        <span></span><span></span><span></span>
+      </span>
+    </button>
+    <transition name="fade-overlay">
+      <div v-if="sidebarOpen" class="sidebar-overlay" @click="sidebarOpen = false" />
+    </transition>
+    <AppSidebar :is-open="sidebarOpen" @close="sidebarOpen = false" />
     <main class="app-main">
       <router-view />
     </main>
@@ -8,5 +16,11 @@
 </template>
 
 <script setup>
+import { ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
 import AppSidebar from './components/AppSidebar.vue'
+
+const sidebarOpen = ref(false)
+const route = useRoute()
+watch(() => route.fullPath, () => { sidebarOpen.value = false })
 </script>
